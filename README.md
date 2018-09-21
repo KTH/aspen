2908a2b56066811ab778c601b1ec9a1fe80ca532
Jens Tinglev <tinglev@kth.se>
2018-09-21 18:10:50 +0200

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # aspen
+
 Cellus 2.0
+
+## Hard coded requirements
+
+* App password file must look like this:
+
+```yaml
+passwords:
+    app1: pwd1
+    app2: pwd2
+    ...
+```
+
+* Docker stack files must be named `docker-stack.yml`
--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 __author__ = 'tinglev@kth.se'
 
-import modules.util.log as log_module
+from modules.util import log
+from modules.pipelines.master_pipeline import MasterPipeline
+
+def main():
+    log.init_logging()
+    pipeline = MasterPipeline()
+    pipeline.run_pipeline()
 
 if __name__ == '__main__':
-    log_module.init_logging()
+    main()
