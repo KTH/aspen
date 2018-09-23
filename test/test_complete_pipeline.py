fdfb3a0b539d23f2ef7fb37b2495c872901bec11
Jens Tinglev <tinglev@kth.se>
2018-09-23 19:09:13 +0200

    Naming fix

--- test/test_complete_pipeline.py
+++ test/test_complete_pipeline.py
@@ -9,17 +9,17 @@ from modules.util import environment
 class TestEntirePipeline(unittest.TestCase):
 
     def test_entire_pipeline(self):
-        rp = root_path.PROJECT_ROOT
+        r_path = root_path.PROJECT_ROOT
         # Initialize test environment
-        os.environ[environment.APP_PWD_FILE_PATH] = os.path.join(rp, 'test/app.passwords.yml')
+        os.environ[environment.APP_PWD_FILE_PATH] = os.path.join(r_path, 'test/app.passwords.yml')
         os.environ[environment.CLUSTERS_TO_DEPLOY] = 'active,stage'
         os.environ[environment.DOCKER_REGISTRY_PWD] = 'will_be_mocked'
         os.environ[environment.DOCKER_REGISTRY_USER] = 'will_be_mocked'
         os.environ[environment.DOCKER_REGISTRY_URL] = 'localhost/registry'
         os.environ[environment.CLUSTER_STATUS_API_URL] = 'localhost/portillo'
         os.environ[environment.REGISTRY_REPOSITORY_URL] = 'localhost/cellus-registry'
-        os.environ[environment.REGISTRY_SUB_DIRECTORY] = os.path.join(rp, 'test/registry_repo')
-        os.environ[environment.VAULT_KEY_PATH] = os.path.join(rp, 'test/vault.key')
+        os.environ[environment.REGISTRY_SUB_DIRECTORY] = os.path.join(r_path, 'test/registry_repo')
+        os.environ[environment.VAULT_KEY_PATH] = os.path.join(r_path, 'test/vault.key')
         pipeline = AspenPipeline()
         pipeline.run_pipeline()
 
