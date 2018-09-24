a511dfe09aab83303b7667a9e8d339bd03dcf6db
Jens Tinglev <tinglev@kth.se>
2018-09-24 20:44:26 +0200

    Deployment now done in parallell

--- modules/steps/start_deployment_pipelines.py
+++ modules/steps/start_deployment_pipelines.py
@@ -1,5 +1,6 @@
 __author__ = 'tinglev@kth.se'
 
+import asyncio
 from modules.steps.base_pipeline_step import BasePipelineStep
 from modules.pipelines.deployment_pipeline import DeploymentPipeline
 from modules.util import data_defs
@@ -13,9 +14,17 @@ class StartDeploymentPipelines(BasePipelineStep):
         return [data_defs.STACK_FILES]
 
     def run_step(self, pipeline_data):
+        loop = asyncio.get_event_loop()
+        tasks = []
         for file_path in pipeline_data[data_defs.STACK_FILES]:
-            deployment_pipeline = DeploymentPipeline()
-            pipeline_data = {data_defs.STACK_FILE_PATH: file_path}
-            deployment_pipeline.set_pipeline_data(pipeline_data)
-            deployment_pipeline.run_pipeline()
+            tasks.append(asyncio.ensure_future(self.init_and_run(pipeline_data, file_path)))
+        loop.run_until_complete(asyncio.wait(tasks))
+        loop.close()
         return pipeline_data
+
+    @asyncio.coroutine
+    def init_and_run(self, pipeline_data, file_path):
+        deployment_pipeline = DeploymentPipeline()
+        pipeline_data = {data_defs.STACK_FILE_PATH: file_path}
+        deployment_pipeline.set_pipeline_data(pipeline_data)
+        deployment_pipeline.run_pipeline()
