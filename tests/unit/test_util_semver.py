e6649d1a2c3955e28b24147ba4aa204c891dcbc2
tinglev <tinglev@kth.se>
2018-09-26 13:11:30 +0200

    More unittests

---
+++ tests/unit/test_util_semver.py
@@ -0,0 +1,10 @@
+__author__ = 'tinglev@kth.se'
+
+import unittest
+from modules.steps.secret_verification import SecretVerification
+from modules.util import semver
+
+class TestUtilSemver(unittest.TestCase):
+
+    def test_create_pipeline_from_array(self):
+        pass
