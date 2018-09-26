b17d772c0b3be0880000e5f237b8e0e333b8694f
Jens Tinglev <tinglev@kth.se>
2018-09-26 18:28:14 +0200

    Added reJSON startup to test runs

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Start local reJSON
+docker run -d -p 6379:6379 --name redis-rejson redislabs/rejson:latest
+
 # Run with "--debug" to show program logs
-pipenv run green -vv --run-coverage --failfast --file-pattern "integration_*" "tests/integration" "$@"
+REDIS_URL=localhost:6379 pipenv run green -vv --run-coverage --failfast --file-pattern "integration_*" "tests/integration" "$@"
+
+docker stop redis-rejson
+docker rm -f redis-rejson
