54bdefca46be76698e9ed1d82cc0031b5d6fa7b8
Jens Tinglev <tinglev@kth.se>
2018-10-11 14:24:11 +0200

    Subprocess should now run threaded

--- modules/util/process.py
+++ modules/util/process.py
@@ -7,13 +7,16 @@ __author__ = 'tinglev@kth.se'
 import subprocess
 import logging
 
-LOGGER = logging.getLogger(__name__)
-
 def run_with_output(cmd):
     try:
-        LOGGER.debug('Running command with output: "%s"', cmd)
-        return subprocess.check_output(f'{cmd}', stderr=subprocess.STDOUT,
-                                       shell=True, close_fds=True)
+        logger = logging.getLogger(__name__)
+        logger.debug('Running command with output: "%s"', cmd)
+        completed_process = subprocess.run(f'{cmd}', shell=True,
+                                           stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE)
+        if completed_process.stderr:
+            return completed_process.stderr
+        return completed_process.stdout
     except subprocess.CalledProcessError as cpe:
-        LOGGER.error('Shell command gave error with output "%s"', str(cpe.output).rstrip('\n'))
+        logger.error('Shell command gave error with output "%s"', str(cpe.output).rstrip('\n'))
         raise
