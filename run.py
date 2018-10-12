1e8d1c5f8960df5a31852be396610237ec77c701
Jens Tinglev <tinglev@kth.se>
2018-10-12 21:26:00 +0200

    Threaded sync

--- run.py
+++ run.py
@@ -2,11 +2,36 @@ __author__ = 'tinglev@kth.se'
 
 import time
 import logging
-from flask import Flask
+from threading import Thread, Event
+from flask import Flask, jsonify
 from modules.util import log, redis
 from modules.pipelines.aspen_pipeline import AspenPipeline
 
 FLASK_APP = Flask(__name__)
+
+class SyncThread(Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self, target):
+        super(SyncThread, self).__init__(target=target)
+        self._stop_event = Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+SYNC_THREAD = SyncThread(target=sync_routine)
+
+def sync_routine():
+    logger = logging.getLogger(__name__)
+    pipeline = AspenPipeline()
+    while not SYNC_THREAD.stopped():
+        pipeline.run_pipeline()
+        logger.info('Main pipeline done, waiting 15 seconds before next run')
+        time.sleep(15)
 
 @FLASK_APP.route('/api/v1/cache', methods=['DEL'])
 def clear_cache():
@@ -14,15 +39,22 @@ def clear_cache():
     client = redis.get_client()
     client.flushdb()
     logger.info('Cleared redis cache')
+    return jsonify(message='Cache cleared')
 
-@FLASK_APP.route('/api/v1/sync', methods=['PUT'])
+@FLASK_APP.route('/api/v1/sync/start', methods=['GET'])
 def start_sync():
     logger = logging.getLogger(__name__)
-    pipeline = AspenPipeline()
-    while True:
-        pipeline.run_pipeline()
-        logger.info('Main pipeline done, waiting 15 seconds before next run')
-        time.sleep(15)   
+    logger.info('Starting sync thread')
+    SYNC_THREAD.run()
+    return jsonify(message='Sync thread started')
+
+@FLASK_APP.route('/api/v1/sync/stop', methods=['GET'])
+def stop_sync():
+    logger = logging.getLogger(__name__)
+    logger.info('Stopping sync thread')
+    if not SYNC_THREAD.stopped():
+        SYNC_THREAD.stop()
+    return jsonify(message='Sync thread stopped')
 
 def main():
     log.init_logging()
