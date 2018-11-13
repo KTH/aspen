67e21807e2539ba6cec5bb0ec2d81379bf8fe939
Jens Tinglev <tinglev@kth.se>
2018-11-13 10:11:28 +0100

    Added EXCLUDED_APPS

---
+++ modules/steps/stop_excluded_apps.py
@@ -0,0 +1,20 @@
+__author__ = 'tinglev@kth.se'
+
+from modules.steps.base_pipeline_step import BasePipelineStep
+from modules.util import environment, data_defs
+
+class StopExcludedApps(BasePipelineStep):
+
+    def get_required_env_variables(self):
+        return []
+
+    def get_required_data_keys(self):
+        return [data_defs.APPLICATION_NAME]
+
+    def run_step(self, pipeline_data):
+        excluded_apps = environment.get_env_list(environment.EXCLUDED_APPS)
+        application_name = pipeline_data[data_defs.APPLICATION_NAME]
+        if application_name in excluded_apps:
+            self.log.info('Stopping due to app being excluded: "%s"', application_name)
+            self.stop_pipeline()
+        return pipeline_data
---
+++ test/unit/test_stop_excluded_apps.py
@@ -0,0 +1,27 @@
+__author__ = 'tinglev@kth.se'
+
+import os
+import unittest
+from unittest.mock import Mock
+from test import mock_test_data
+from modules.steps.stop_excluded_apps import StopExcludedApps
+from modules.util import environment
+
+class TestStopExcludedApps(unittest.TestCase):
+
+    def test_run_step(self):
+        step = StopExcludedApps()
+        step.stop_pipeline = Mock()
+        pipeline_data = mock_test_data.get_pipeline_data()
+        os.environ[environment.EXCLUDED_APPS] = "app1,app2"
+        step.run_step(pipeline_data)
+        step.stop_pipeline.assert_not_called()
+        step.stop_pipeline.reset_mock()
+        os.environ[environment.EXCLUDED_APPS] = ""
+        step.run_step(pipeline_data)
+        step.stop_pipeline.assert_not_called()
+        step.stop_pipeline.reset_mock()
+        os.environ[environment.EXCLUDED_APPS] = "app1,kth-azure-app"
+        step.run_step(pipeline_data)
+        step.stop_pipeline.assert_called_once()
+     
