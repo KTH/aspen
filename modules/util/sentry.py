f3e6d2e2bd1b28a5066189947c3e83fb33347fe7
Jens Tinglev <tinglev@kth.se>
2018-11-22 12:10:29 +0100

    Fixed sentry

--- modules/util/sentry.py
+++ modules/util/sentry.py
@@ -4,23 +4,18 @@ import logging
 import sentry_sdk
 from modules.util import environment
 
-_INITIALIZED = False
-
 def init():
-    global _INITIALIZED # pylint: disable=W0603
     log = logging.getLogger(__name__)
     sentry_dsn = environment.get_env(environment.SENTRY_DSN)
-    if sentry_dsn:
-        log.info('Initializing Sentry with dsn: "%s"', sentry_dsn)
-        sentry_sdk.init(sentry_dsn, debug=True)
-        _INITIALIZED = True
+    log.info('Initializing Sentry with dsn: "%s"', sentry_dsn)
+    sentry_sdk.init(sentry_dsn, debug=True)
 
 def capture_exception(exception):
-    global _INITIALIZED # pylint: disable=W0603
-    if _INITIALIZED:
-        sentry_sdk.capture_exception(exception)
+    log = logging.getLogger(__name__)
+    log.info('Caputing Sentry exception')
+    sentry_sdk.capture_exception(exception)
 
 def capture_message(message):
-    global _INITIALIZED # pylint: disable=W0603
-    if _INITIALIZED:
-        sentry_sdk.capture_message(message)
+    log = logging.getLogger(__name__)
+    log.debug('Caputing Sentry message')
+    sentry_sdk.capture_message(message)
