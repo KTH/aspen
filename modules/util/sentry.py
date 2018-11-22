85d6605e77a629b038a0382958afecebb18e4288
Jens Tinglev <tinglev@kth.se>
2018-11-22 12:24:04 +0100

    Run sentry init on each capture

--- modules/util/sentry.py
+++ modules/util/sentry.py
@@ -1,21 +1,14 @@
 __author__ = 'tinglev@kth.se'
 
-import logging
 import sentry_sdk
 from modules.util import environment
 
-def init():
-    log = logging.getLogger(__name__)
-    sentry_dsn = environment.get_env(environment.SENTRY_DSN)
-    log.info('Initializing Sentry with dsn: "%s"', sentry_dsn)
-    sentry_sdk.init(sentry_dsn, debug=True)
-
 def capture_exception(exception):
-    log = logging.getLogger(__name__)
-    log.info('Caputing Sentry exception')
+    sentry_dsn = environment.get_env(environment.SENTRY_DSN)
+    sentry_sdk.init(sentry_dsn, debug=True)
     sentry_sdk.capture_exception(exception)
 
 def capture_message(message):
-    log = logging.getLogger(__name__)
-    log.info('Caputing Sentry message')
+    sentry_dsn = environment.get_env(environment.SENTRY_DSN)
+    sentry_sdk.init(sentry_dsn, debug=True)
     sentry_sdk.capture_message(message)
