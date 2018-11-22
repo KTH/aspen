be317b424882f6362970b9dfa11c4e97569ad5aa
Jens Tinglev <tinglev@kth.se>
2018-11-22 11:37:01 +0100

    Fixed sentry typo

--- modules/util/sentry.py
+++ modules/util/sentry.py
@@ -1,12 +1,12 @@
 __author__ = 'tinglev@kth.se'
 
-import sentry-sdk
+import sentry_sdk
 from modules.util import environment
 
-sentry-sdk.init(environment.get_env(environment.SENTRY_DSN))
+sentry_sdk.init(environment.get_env(environment.SENTRY_DSN))
 
 def capture_exception(exception):
-    sentry-sdk.capture_exception(exception)
+    sentry_sdk.capture_exception(exception)
 
 def capture_message(message):
-    sentry-sdk.capture_message(message)
+    sentry_sdk.capture_message(message)
