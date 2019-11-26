611ed5f3fcb79373289e41089340e306f0bfdcdc
Jens Tinglev <tinglev@kth.se>
2019-11-26 08:17:01 +0100

    Fixed missing CLUSTERS_TO_DEPLOY

--- modules/steps/start_deployment_pipelines.py
+++ modules/steps/start_deployment_pipelines.py
@@ -6,6 +6,7 @@ several deployment pipelines in parallell"""
 __author__ = 'tinglev@kth.se'
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from modules.util.data_defs import CLUSTERS_TO_DEPLOY
 from modules.steps.base_pipeline_step import BasePipelineStep
 from modules.pipelines.deployment_pipeline import DeploymentPipeline
 from modules.util import data_defs, environment, thread
@@ -26,7 +27,6 @@ class StartDeploymentPipelines(BasePipelineStep):
         parallelism = environment.get_with_default_int(environment.PARALLELISM, 5)
         nr_of_stack_files = len(pipeline_data[data_defs.STACK_FILES])
         self.log.debug('Running async processing of %s stack files', nr_of_stack_files)
-
         # Sequential version
         #for stack_file in pipeline_data[data_defs.STACK_FILES]:
         #    self.init_and_run(pipeline_data, stack_file)
@@ -53,16 +53,19 @@ class StartDeploymentPipelines(BasePipelineStep):
         deployment_pipeline = DeploymentPipeline()
         pipeline_data = self.init_deploy_pipeline_data(pipeline_data, file_path)
         deployment_pipeline.set_pipeline_data(pipeline_data)
+        print('pld: %s', pipeline_data)
         pipeline_data = deployment_pipeline.run_pipeline()
         return pipeline_data
 
     def init_deploy_pipeline_data(self, pipeline_data, file_path):
         app_passwords = pipeline_data[data_defs.APPLICATION_PASSWORDS]
         cluster_lb_ips = pipeline_data[data_defs.DOCKER_HOST_IPS]
+        clusters_to_deploy = pipeline_data[data_defs.CLUSTERS_TO_DEPLOY]
         pipeline_data = {
             data_defs.STACK_FILE_PATH: file_path,
             data_defs.APPLICATION_PASSWORDS: app_passwords,
-            data_defs.DOCKER_HOST_IPS: cluster_lb_ips
+            data_defs.DOCKER_HOST_IPS: cluster_lb_ips,
+            data_defs.CLUSTERS_TO_DEPLOY: clusters_to_deploy
         }
         pipeline_data[data_defs.DEPLOYMENTS_LAST_RUN] = 0
         return pipeline_data
